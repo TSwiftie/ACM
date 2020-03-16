c6730b8f67f35cb42260643e506406de5730da53
TSwiftie <2224273204@qq.com>
2020-03-16 16:18:21 +0800

    update

---
+++ nowcoder/Train59/a.cpp
@@ -0,0 +1,46 @@
+/************************************************************
+	> File Name: a.cpp
+	> Author: TSwiftie
+	> Mail: 2224273204@qq.com 
+	> Created Time: Mon 16 Mar 2020 04:14:02 PM CST
+************************************************************/
+
+#pragma GCC optimize(2)
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define lowbit(x) (x&-x)
+#define range(x) x.begin(),x.end()
+#define lc (o<<1)
+#define rc (o<<1|1)
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+const int INF = 0x3f3f3f3f;
+const ll INF_ll = 0x3f3f3f3f3f3f3f3fLL;
+const int MAXN = 1e5+5;
+const int MAXM = 2e5+5;
+const int MOD = 1e9+7;
+const int dir[4][2] = {1,0,-1,0,0,1,0,-1};
+const double PI = acos(-1.0);
+const double EPS = 1e-8;
+char jo[] = "XiaoQiao";
+char hui[] = "XiaoHuiHui";
+char ch[MAXN];
+signed main(void){
+	scanf("%s",ch);
+	int len = strlen(ch);
+	int j1 = 0,h1 = 0;
+	for(int i = 0;i < len;i++){
+		if(ch[i]==jo[j1]) j1++;
+		if(ch[i]==hui[h1]) h1++;
+	}
+	if(j1==8&&h1==10) printf("Happy\n");
+	else printf("emm\n");
+	return 0;
+}
