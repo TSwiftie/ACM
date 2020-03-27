22f3052808c4202fef33d809bba51389f0ecea1b
TSwiftie <2224273204@qq.com>
2020-03-27 10:44:04 +0800

    update

---
+++ cf/629Div3/a.cpp
@@ -0,0 +1,39 @@
+/************************************************************
+	> File Name: a.cpp
+	> Author: TSwiftie
+	> Mail: 2224273204@qq.com 
+	> Created Time: Fri 27 Mar 2020 10:33:22 AM CST
+************************************************************/
+
+#include <cstdio>
+#pragma GCC optimize(2)
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define lowbit(x) (x&-x)
+#define SZ(x) ((int)x.size())
+#define all(x) x.begin(),x.end()
+#define lc (o<<1)
+#define rc (o<<1|1)
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+const int INF = 0x3f3f3f3f;
+const ll INF_ll = 0x3f3f3f3f3f3f3f3fLL;
+const double PI = acos(-1.0);
+const double EPS = 1e-8;
+signed main(void){
+	int _;
+	for(scanf("%d",&_);_;_--){
+		ll a, b;
+		scanf("%lld %lld",&a,&b);
+		ll div = ceil(1.0*a/b);
+		printf("%lld\n",div*b-a);
+	}
+	return 0;
+}
