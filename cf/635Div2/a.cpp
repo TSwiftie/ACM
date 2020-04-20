e6eb7ae50ea46ed14b90aff50ab3872854166800
TSwiftie <2224273204@qq.com>
2020-04-20 16:17:47 +0800

    update

---
+++ cf/635Div2/a.cpp
@@ -0,0 +1,37 @@
+/************************************************************
+	> File Name: a.cpp
+	> Author: TSwiftie
+	> Mail: 2224273204@qq.com 
+	> Created Time: Mon 20 Apr 2020 04:07:45 PM CST
+************************************************************/
+
+#pragma GCC optimize(2)
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define lowbit(x) (x&-x)
+#define SZ(x) ((int)x.size())
+#define all(x) x.begin(),x.end()
+#define lc (o<<1)
+#define rc (o<<1|1)
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+const int INF = 0x3f3f3f3f;
+const ll INF_ll = 0x3f3f3f3f3f3f3f3fLL;
+const double PI = acos(-1.0);
+const double EPS = 1e-8;
+signed main(void){
+	int t;
+	for(scanf("%d",&t);t--;){
+		ll a, b, c, d;
+		scanf("%lld%lld%lld%lld",&a,&b,&c,&d);
+		printf("%lld %lld %lld\n",b,c,c);
+	}
+	return 0;
+}
