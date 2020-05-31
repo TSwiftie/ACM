eda3f15ce9decb8d1527e4bab858b22c08e44bb1
TSwiftie <2224273204@qq.com>
2020-05-31 21:35:41 +0800

    update

--- atcoder/abc169/e.cpp
+++ atcoder/abc169/e.cpp
@@ -28,9 +28,61 @@ const double PI = acos(-1.0);
 const double EPS = 1e-8;
 const int N = 2e5+10;
 int a[N],b[N],n;
+int gcd1(int a,int b){
+	return b?gcd1(b,a%b):a;
+}
+int mul(int a,int b,int p){
+	int res = 0;
+	while(b){
+		if(b&1) res = (res + a) % p;
+		a = (a+a)%p;
+		b >>= 1;
+	}
+	return b;
+}
+int ksm(int a,int b,int p){
+	int res = 1;
+	while(b){
+		if(b&1) res = a * res % p;
+		a = a * a % p;
+		b >>= 1;
+	}
+	return res;
+}
+int ksm1(int a,int b,int p){
+	int res = 1;
+	while(b){
+		if(b&1) res = a * res % p;
+		a = a * a % p;
+		b >>= 1;
+	}
+	return res;
+}
+int ksm2(int a,int b,int p){
+	int res = 1;
+	while(b){
+		if(b&1) res = a * res % p;
+		a = a * a % p;
+		b >>= 1;
+	}
+	return res;
+}
+int ksm3(int a,int b,int p){
+	int res = 1;
+	while(b){
+		if(b&1) res = a * res % p;
+		a = a * a % p;
+		b >>= 1;
+	}
+	return res;
+}
 signed main(void){
 	IOS;
 	cin >> n;
-	for(int i = 1;i <= n;++i) cin >> a[i] >> b[i];
+	for(int i = 0;i < n;++i) cin >> a[i] >> b[i];
+	sort(a,a+n);
+	sort(b,b+n);
+	if(n&1) cout << b[n/2]-a[n/2]+1 << endl;
+	else cout << b[(n-1)/2]+b[(n+1)/2]-a[(n-1)/2]-a[(n+1)/2]+1 << endl;
 	return 0;
 }
