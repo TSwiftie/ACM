996be0e7b3923cd6603a30134a408d1914630aab
TSwiftie <2224273204@qq.com>
2020-06-02 16:29:44 +0800

    update

---
+++ nowcoder/2019mult-2/h.cpp
@@ -0,0 +1,37 @@
+/************************************************************
+	> File Name: h.cpp
+	> Author: TSwiftie
+	> Mail: 2224273204@qq.com 
+	> Created Time: Tue 02 Jun 2020 04:28:30 PM CST
+************************************************************/
+#pragma GCC optimize(2)
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define lowbit(x) (x&-x)
+#define SZ(x) ((int)x.size())
+#define all(x) x.begin(),x.end()
+#define lc (o<<1)
+#define rc (o<<1|1)
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+typedef long long LL;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<LL> vll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<LL,LL> pll;
+const int INF = 0x3f3f3f3f;
+const LL INF_ll = 0x3f3f3f3f3f3f3f3fLL;
+const double PI = acos(-1.0);
+const double EPS = 1e-8;
+const int N = 1e3+10;
+int n, m;
+char t[N];
+int a[N][N];
+int mx1,mx2;
+int s[N], p, w[N];
+signed main(void){
+	return 0;
+}
