8d2e36797ae56f6979fb34083ef1579f4223b5b3
TSwiftie <2224273204@qq.com>
2020-06-16 20:00:26 +0800

    update

---
+++ LG/6159.cpp
@@ -0,0 +1,35 @@
+/************************************************************
+	> File Name: 6159.cpp
+	> Author: TSwiftie
+	> Mail: tswiftie@foxmail.com 
+	> Created Time: Tue 16 Jun 2020 07:37:56 PM CST
+************************************************************/
+#pragma GCC optimize(2)
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define int long long
+#define lowbit(x) (x&-x)
+#define SZ(x) ((int)x.size())
+#define all(x) x.begin(),x.end()
+#define lc (o<<1)
+#define rc (o<<1|1)
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+typedef long long LL;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<LL> vll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<LL,LL> pll;
+const int INF = 0x3f3f3f3f;
+const LL INF_ll = 0x3f3f3f3f3f3f3f3fLL;
+const double PI = acos(-1.0);
+const double EPS = 1e-8;
+signed main(void){
+	int n, p, k;
+	cin >> n >> p >> k;
+	cout << p * k % n << endl;
+	return 0;
+}
