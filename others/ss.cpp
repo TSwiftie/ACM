6a598a090b66bff07756dffd04485df54f12c3b9
TSwiftie <2224273204@qq.com>
2020-06-16 17:17:44 +0800

    update

--- others/ss.cpp
+++ others/ss.cpp
@@ -1,8 +1,8 @@
 /************************************************************
 	> File Name: ss.cpp
 	> Author: TSwiftie
-	> Mail: 2224273204@qq.com 
-	> Created Time: Thu 21 May 2020 10:10:53 AM CST
+	> Mail: tswiftie@foxmail.com 
+	> Created Time: Tue 16 Jun 2020 05:17:32 PM CST
 ************************************************************/
 #pragma GCC optimize(2)
 #include <bits/stdc++.h>
@@ -26,34 +26,6 @@ const int INF = 0x3f3f3f3f;
 const LL INF_ll = 0x3f3f3f3f3f3f3f3fLL;
 const double PI = acos(-1.0);
 const double EPS = 1e-8;
-double f(int n){
-	if(n==1) return 1.0;
-	else if(n&1) return f(n-1)+1;
-	else return f(n-1)/2;
-}
 signed main(void){
-	double ans = 0;
-	int n;
-	scanf("%d",&n);
-	for(int i = 1;i <= 2*n;i++){
-		if(i&1) ans += 1;
-		else ans /= 2;
-	}
-	ans = 0;
-	for(int i = 1;i <= 2*n;i++){
-		if(i<=n) ans += 1;
-		else ans /= 2;
-	}
-	printf("%lf\n",ans);
-	ans = 0;
-	int count = 0;
-	for(int i = 1;i < 10;i++){
-		if(i <= 5){
-			ans = pow(2,i);
-			count++;
-		}
-		ans = count/ans;
-	}
-	printf("%lf\n",ans);
 	return 0;
 }
