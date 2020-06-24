a6fb7aade29788c84c96d73b7b3c923e40fadf2b
TSwiftie <2224273204@qq.com>
2020-06-24 11:19:25 +0800

    update

--- cf/652Div2/c.cpp
+++ cf/652Div2/c.cpp
@@ -7,6 +7,7 @@
 #pragma GCC optimize(2)
 #include <bits/stdc++.h>
 #include <ext/rope>
+#define int long long
 #define lowbit(x) (x&-x)
 #define SZ(x) ((int)x.size())
 #define all(x) x.begin(),x.end()
@@ -27,7 +28,22 @@ const int INF = 0x3f3f3f3f;
 const LL INF_ll = 0x3f3f3f3f3f3f3f3fLL;
 const double PI = acos(-1.0);
 const double EPS = 1e-8;
-int t, n;
+const int N = 2e5+10;
+int t, n, k, a[N], w[N];
 signed main(void){
+	IOS;
+	for(cin >> t;t--;){
+		cin >> n >> k;
+		for(int i = 0;i < n;++i) cin >> a[i];
+		for(int i = 0;i < k;++i) cin >> w[i],w[i]--;
+		sort(a,a+n,greater<int>());
+		sort(w,w+k);
+		int s = accumulate(a,a+k,0LL);
+		for(int i = 0,j = k-1;i < k;++i){
+			if(!w[i]) s += a[i];
+			else j += w[i], s += a[j];
+		}
+		cout << s << endl;
+	}
 	return 0;
 }
