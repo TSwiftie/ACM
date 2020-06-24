800ad4e0ab061c6b56d11a09768d2ee70a0c213e
TSwiftie <2224273204@qq.com>
2020-06-24 09:16:45 +0800

    update

---
+++ cf/652Div2/b.cpp
@@ -0,0 +1,33 @@
+/************************************************************
+	> File Name: b.cpp
+	> Author: TSwiftie
+	> Mail: tswiftie@foxmail.com 
+	> Created Time: Wed 24 Jun 2020 09:14:52 AM CST
+************************************************************/
+#pragma GCC optimize(2)
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define lowbit(x) (x&-x)
+#define SZ(x) ((int)x.size())
+#define all(x) x.begin(),x.end()
+#define lc (o<<1)
+#define rc (o<<1|1)
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+mt19937 rnd(time(0));
+typedef long long LL;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<LL> vll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<LL,LL> pll;
+const int INF = 0x3f3f3f3f;
+const LL INF_ll = 0x3f3f3f3f3f3f3f3fLL;
+const double PI = acos(-1.0);
+const double EPS = 1e-8;
+int t;
+signed main(void){
+	return 0;
+}
