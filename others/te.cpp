148342e55edf746dc81e6135b38cdc3ee0125ec0
TSwiftie <2224273204@qq.com>
2020-06-27 10:20:46 +0800

    update

--- others/te.cpp
+++ others/te.cpp
@@ -1,41 +1,50 @@
-#include <iostream>
-#include <algorithm>
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <cmath>
+/************************************************************
+	> File Name: hosrspool.cpp
+	> Author: TSwiftie
+	> Mail: tswiftie@foxmail.com 
+	> Created Time: Sat 27 Jun 2020 09:06:05 AM CST
+************************************************************/
+#pragma GCC optimize(2)
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define lowbit(x) (x&-x)
+#define SZ(x) ((int)x.size())
+#define all(x) x.begin(),x.end()
+#define lc (o<<1)
+#define rc (o<<1|1)
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
 using namespace std;
-const int N = 1e6+7;
-const int inf = 0x3f3f3f3f;
-int tb[30];
-int bm(char str[], char text[])
-{
-    int ls = strlen(str), lt = strlen(text);
-    for(int i = 0; i < ls - 1; i++)
-    {
-        tb[str[i] - 'A'] = ls - i - 1;
-    }
-    int p = ls - 1;
-    while(p < lt)
-    {
+typedef long long LL;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<LL> vll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<LL,LL> pll;
+const int INF = 0x3f3f3f3f;
+const LL INF_ll = 0x3f3f3f3f3f3f3f3fLL;
+const double PI = acos(-1.0);
+const double EPS = 1e-8;
+const int N = 7e5+10;
+int tab[355];
+char ch[N],sh[N];
+int Find(char *s,char *t,int n,int m){
+    for(int i = 0;i < 255;++i) tab[i] = m;
+    for(int i = 0;i < m-1;++i) tab[t[i]] = m-1-i;
+    int i = m-1;
+    while(i <= n-1){
         int k = 0;
-        while(str[ls - k - 1] == text[p - k] && k <= ls)
-        {
-            k++;
-        }
-        if(k == ls)
-        {
-            return p - ls + 1;
-        }
-        else p += tb[text[p] - 'A'];
+        while(k <= m-1 && t[m-1-k]==s[i-k]) ++k;
+        if(k==m) return i-m+1;
+        else i += tab[s[i]];
     }
     return -1;
 }
-int main()
-{
-    char str[N], text[N];
-    cin >> str >> text;
-    cout << bm(str, text) << endl;
+signed main(void){
+    scanf("%s%s",sh,ch);
+    int n = strlen(ch);
+    int m = strlen(sh);
+    printf("%d\n",Find(ch,sh,n,m));
     return 0;
 }
-
