27250e125cb7d836e2cd79ba176ac45c6cf400a8
TSwiftie <2224273204@qq.com>
2020-07-08 20:30:11 +0800

    update

--- cf/Global9/b.cpp
+++ cf/Global9/b.cpp
@@ -17,7 +17,7 @@ using namespace std;
 //mt19937 rnd(time(0));
 typedef long long LL;
 typedef long long ll;
-typedef unsigned long long ull;
+typedef unsigned long long ull; 
 typedef vector<int> vi;
 typedef vector<LL> vll;
 typedef long double ld;
@@ -31,12 +31,28 @@ const int N = 304;
 int a[N][N], n, m, t;
 signed main(void){
     for(scanf("%d",&t);t--;){
+        bool ok = true;
         scanf("%d%d",&n,&m);
         for(int i = 1;i <= n;++i){
             for(int j = 1;j <= m;++j){
                 scanf("%d",&a[i][j]);
+                if(((i==1&&j==1)||(i==1&&j==m)||(i==n&&j==1)||(i==n&&j==m))&&a[i][j]>2) ok = false;
+                else if((i==1 || i==n) && a[i][j] > 3) ok = false;
+                else if((j==1 || j==m) && a[i][j] > 3) ok = false;
+                else if(a[i][j] > 4) ok = false;
             }
         }
+        if(ok){
+            printf("YES\n");
+            for(int i = 1;i <= n;++i)
+                for(int j = 1;j <= m;++j){
+                    if((i==1&&j==1)||(i==1&&j==m)||(i==n&&j==1)||(i==n&&j==m)) printf("2");
+                    else if((i==1 || i==n)) printf("3");
+                    else if((j==1 || j==m)) printf("3");
+                    else printf("4");
+                    printf("%c"," \n"[j==m]);
+                }
+        }else printf("NO\n");
     }
     return 0;
 }
