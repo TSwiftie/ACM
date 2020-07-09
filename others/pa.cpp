b2204a55235bc69850292fa2d589cfd7a436fb6a
TSwiftie <2224273204@qq.com>
2020-07-09 11:03:48 +0800

    update

---
+++ others/pa.cpp
@@ -0,0 +1,37 @@
+/************************************************************
+	> File Name: pa.cpp
+	> Author: TSwiftie
+	> Mail: tswiftie@foxmail.com 
+	> Created Time: jue 09 jul 2020 11:01:34
+************************************************************/
+#pragma GCC optimize(2)
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define lowbit(x) (x&-x)
+#define SZ(x) ((int)x.size())
+#define all(x) x.begin(),x.end()
+#define lc (o<<1)
+#define rc (o<<1|1)
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+//mt19937 rnd(time(0));
+typedef long long LL;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<LL> vll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<LL,LL> pll;
+const int INF = 0x3f3f3f3f;
+const LL INF_ll = 0x3f3f3f3f3f3f3f3fLL;
+const double PI = acos(-1.0);
+const double EPS = 1e-8;
+bool judge(string str){
+    for(int i = 0,j = str.length()-1;i < j;++i,--j)
+        if(str[i]!=str[j]) return false;
+    return true;
+}
+signed main(void){
+    return 0;
+}
