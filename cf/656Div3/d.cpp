7a4bf439724088c2decec541a19b6c98917b7b2d
TSwiftie <2224273204@qq.com>
2020-07-19 12:40:22 +0800

    update

--- cf/656Div3/d.cpp
+++ cf/656Div3/d.cpp
@@ -1,8 +1,8 @@
 /************************************************************
-	> File Name: c.cpp
+	> File Name: d.cpp
 	> Author: TSwiftie
 	> Mail: tswiftie@foxmail.com 
-	> Created Time: Sun 19 Jul 2020 11:02:10 AM CST
+	> Created Time: Sun 19 Jul 2020 11:28:28 AM CST
 ************************************************************/
 #pragma GCC optimize(2)
 #include <bits/stdc++.h>
@@ -27,17 +27,23 @@ const int INF = 0x3f3f3f3f;
 const LL INF_ll = 0x3f3f3f3f3f3f3f3fLL;
 const double PI = acos(-1.0);
 const double EPS = 1e-8;
-const int N = 2e5+10;
-int a[N];
+const int N = 131100;
+char ch[N];
+int t, n;
+int dfs(int st,int ed,char ap){
+    if(st==ed) return ch[st]==ap ? 0 : 1;
+    int pre = 0,suf = 0,mid = st + (ed-st+1)/2;
+    for(int i = st;i < mid;++i)
+        if(ch[i]!=ap) ++pre;
+    for(int i = mid;i <= ed;++i)
+        if(ch[i]!=ap) ++suf;
+    return min(pre+dfs(mid,ed,ap+1),suf+dfs(st,mid-1,ap+1));
+}
 signed main(void){
-    int t, n;
     for(scanf("%d",&t);t--;){
         scanf("%d",&n);
-        for(int i = 0;i < n;++i) scanf("%d",a+i);
-        int pos = n-1;
-        while(pos > 0 && a[pos-1] >= a[pos]) --pos;
-        while(pos > 0 && a[pos-1] <= a[pos]) --pos;
-        printf("%d\n",pos);
+        scanf("%s",ch+1);
+        printf("%d\n",dfs(1,n,'a'));
     }
     return 0;
 }
