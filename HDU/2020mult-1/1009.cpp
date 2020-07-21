afee23e3ccd2d7a394c3da76ef8f499efaa0fc29
TSwiftie <2224273204@qq.com>
2020-07-21 18:09:35 +0800

    update

--- HDU/2020mult-1/1009.cpp
+++ HDU/2020mult-1/1009.cpp
@@ -2,11 +2,11 @@
 	> File Name: 1009.cpp
 	> Author: TSwiftie
 	> Mail: tswiftie@foxmail.com 
-	> Created Time: Tue 21 Jul 2020 02:50:47 PM CST
+	> Created Time: Tue 21 Jul 2020 04:47:59 PM CST
 ************************************************************/
 #pragma GCC optimize(2)
 #include <bits/stdc++.h>
-#include <ext/rope>
+#define int long long
 #define lowbit(x) (x&-x)
 #define SZ(x) ((int)x.size())
 #define all(x) x.begin(),x.end()
@@ -29,26 +29,37 @@ const double PI = acos(-1.0);
 const double EPS = 1e-8;
 const int N = 5e4+10;
 struct node{
-    int p,a,id;
+    int p,a;
+    bool operator < (const node &x) const{
+        if(a!=x.a) return a < x.a;
+        return p < x.p;
+    }
 }s[N];
-bool vis[N];
-bool cmp(const node &x,const node &y){
-    if(x.p==y.p) return x.a > y.a;
-    else x.p > y.p;
+map<node,int>mp;
+int sta[N];
+bool judge(node x,node y,node z){
+    return (x.p-y.p) * (z.a-x.a) >= (x.p-z.p) * (y.a-x.a);
 }
 signed main(void){
-    int t, n;
-    for(scanf("%d",&t);t--;){
-        scanf("%d",&n);
+    int t,n;
+    for(scanf("%lld",&t);t--;){
+        scanf("%lld",&n);
         for(int i = 1;i <= n;++i){
-            scanf("%d%d",&s[i].p,&s[i].a);
-            s[i].id = i;
-            vis[i] = false;
+            scanf("%lld%lld",&s[i].p,&s[i].a);
+            ++mp[s[i]];
         }
-        sort(s+1,s+1+n,cmp);
-        for(int i = 2;i <= n;++i){
-            if(s[i].a <= s[1].a || s[i].p == s[1].p) vis[s[i].id] = true;
+        int top = 0;
+        sort(s+1,s+1+n);
+        for(int i = 1;i <= n;++i){
+            while((top > 0 && s[sta[top]].p <= s[i].p)||(top >= 2 && judge(s[sta[top-1]],s[sta[top]],s[i]))) --top;
+            sta[++top] = i;
         }
+        int ans = top;
+        for(int i = 1;i <= top;++i){
+            if(mp[s[sta[i]]] > 1) --ans;
+        }
+        printf("%lld\n",ans);
+        mp.clear();
     }
     return 0;
 }
