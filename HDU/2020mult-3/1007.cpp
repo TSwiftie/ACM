bd83f99d3d0ffee0bf3b265da4a8ba7d5e7e6539
TSwiftie <2224273204@qq.com>
2020-07-30 09:15:07 +0800

    update

--- HDU/2020mult-3/1007.cpp
+++ HDU/2020mult-3/1007.cpp
@@ -27,6 +27,53 @@ const int INF = 0x3f3f3f3f;
 const LL INF_ll = 0x3f3f3f3f3f3f3f3fLL;
 const double PI = acos(-1.0);
 const double EPS = 1e-8;
+const int N = 52;
+int n, m, k, e[N][N], pre[N], res, d[N];
+bool vis[N];
+void Dijkstra(int *pre){
+    memset(vis,false,sizeof vis);
+    memset(d,INF,sizeof d);
+    d[1] = 0;
+    for(int i = 1;i < n;++i){
+        int minx = INF,u = 0;
+        for(int j = 1;j <= n;++j) if(!vis[j] && d[j] < minx) u = j,minx = d[j];
+        vis[u] = true;
+        for(int j = 1;j <= n;++j){
+            if(e[u][j] < INF && !vis[j] && d[j] > d[u] + e[u][j])
+                pre[j] = u,d[j] = d[u] + e[u][j];
+        }
+    }
+}
+void dfs(int dep){
+    if(dep==k+1){
+        Dijkstra(pre);
+        res = max(res,d[n]);
+        return ;
+    }
+    int pre[N] = {0};
+    Dijkstra(pre);
+    int now = n;
+    while(pre[now]){
+        int tmp = e[pre[now]][now];
+        e[pre[now]][now] = e[now][pre[now]] = INF;
+        dfs(dep+1);
+        e[pre[now]][now] = e[now][pre[now]] = tmp;
+        now = pre[now];
+    }
+}
+void solve(){
+    scanf("%d%d",&n,&k);
+    m = n*(n-1)/2;
+    res = 0;
+    for(int i = 1,u,v,w;i <= m;++i){
+        scanf("%d%d%d",&u,&v,&w);
+        e[u][v] = e[v][u] = w;
+    }
+    dfs(1);
+    printf("%d\n",res);
+}
 signed main(void){
+    int T;
+    for(scanf("%d",&T);T--;) solve();
     return 0;
 }
