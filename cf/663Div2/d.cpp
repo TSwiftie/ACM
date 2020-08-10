a285d5f59aa4cd060baa1c6fcfb2a9060584c54f
TSwiftie <2224273204@qq.com>
2020-08-10 11:46:52 +0800

    update

---
+++ cf/663Div2/d.cpp
@@ -0,0 +1,36 @@
+/************************************************************
+	> File Name: d.cpp
+	> Author: TSwiftie
+	> Mail: tswiftie@foxmail.com 
+	> Created Time: Mon 10 Aug 2020 11:32:52 AM CST
+************************************************************/
+#pragma GCC optimize(2)
+#include <bits/stdc++.h>
+#include <ext/rope>
+//#define int long long
+#define lowbit(x) (x&-x)
+#define SZ(x) ((int)x.size())
+#define all(x) x.begin(),x.end()
+#define lc (o<<1)
+#define rc (o<<1|1)
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+//mt19937 rnd(time(0));
+typedef long double ld;
+typedef long long LL;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<LL> vll;
+typedef pair<int,int> pii;
+typedef pair<LL,LL> pll;
+const int INF = 0x3f3f3f3f;
+const LL INF_ll = 0x3f3f3f3f3f3f3f3fLL;
+const double PI = acos(-1.0);
+const double EPS = 1e-8;
+const int N = 1e6+10;
+int n, m;string a[N];
+signed main(void){
+    IOS;cin >> n >> m;for(int i = 0;i < n;++i) cin >> a[i];
+    return 0;
+}
