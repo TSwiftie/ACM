60c21a221f6a57e4891acbd37ef10551a07d9ead
TSwiftie <2224273204@qq.com>
2020-08-12 18:50:38 +0800

    update

--- cf/662Div2/b.cpp
+++ cf/662Div2/b.cpp
@@ -22,6 +22,28 @@ const int INF = 0x3f3f3f3f;
 const LL INF_ll = 0x3f3f3f3f3f3f3f3fLL;
 const double PI = acos(-1.0);
 const double EPS = 1e-8;
+const int N = 1e5+10;
+int cnt[N];
 signed main(void){
+    IOS;int n, q, x, cnt2 = 0,cnt4 = 0;char op;
+    scanf("%d",&n);for(int i = 1;i <= n;++i){
+        scanf("%d",&x);
+        cnt2 -= cnt[x]/2;
+        cnt4 -= cnt[x]/4;
+        ++cnt[x];
+        cnt2 += cnt[x]/2;
+        cnt4 += cnt[x]/4;
+    }
+    for(scanf("%d",&q);q--;){
+        scanf(" %c %d",&op,&x);
+        cnt2 -= cnt[x]/2;
+        cnt4 -= cnt[x]/4;
+        if(op=='+') ++cnt[x];
+        else --cnt[x];
+        cnt2 += cnt[x]/2;
+        cnt4 += cnt[x]/4;
+        if(cnt4 >= 1 && cnt2 >= 4) printf("YES\n");
+        else printf("NO\n");
+    }
     return 0;
 }
