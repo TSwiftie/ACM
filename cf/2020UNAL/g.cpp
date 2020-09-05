81295cc8f5179c6f6bb5544b5ddfbc913f9b3a6c
TSwiftie <2224273204@qq.com>
2020-09-05 12:27:08 +0000

    update

--- cf/2020UNAL/g.cpp
+++ cf/2020UNAL/g.cpp
@@ -22,20 +22,18 @@ const int INF = 0x3f3f3f3f;
 const LL INF_ll = 0x3f3f3f3f3f3f3f3fLL;
 const double PI = acos(-1.0);
 const double EPS = 1e-8;
-int n,root = 1,dis,tmp;
+const int N = 1e5+10;
+const int p = 1e9+7;
+int f[N],n,m;
+int calc(int x){
+    if(x&1) return (x-1)/2%p*x%p;
+    else return x/2%p*(x-1)%p;
+}
+int ksm(int a,int b,int res=1){ for(;b;b>>=1,a = a * a % p) if(b&1) res = res * a % p; return res;}
 signed main(void){
-    IOS;cin >> n;
-    cout << root << endl;
-    cin >> dis;
-    if(dis==0){ cout << "! " << root << endl;return 0; }
-    root <<= 1;
-    while(dis!=0){
-        cout << root << endl;
-        cin >> tmp;
-        if(tmp==0){ cout <<"! " << root << endl;return 0; }
-        if(dis==1 && tmp==2){ cout << "! "<< (root|1) << endl;return 0; }
-        if(tmp > dis) { root|=1;root<<=1;--dis; }
-        else dis = tmp,root<<=1;
-    }
+    IOS;f[2] = 1;for(int i = 4;i < N;i+=2) f[i] = f[i-2] * calc(i) % p;
+    cin >> n >> m;for(int i = 1,x,y;i <= m;++i){ cin >> x >> y; }
+    if(n==2*m) cout << f[2*m] << endl;
+    else cout << f[2*m] * ksm(n-2*m,2*m+1) << endl;
     return 0;
 }
