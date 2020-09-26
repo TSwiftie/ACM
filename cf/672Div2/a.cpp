7ad1141a10ff13f46a67bda3d5ceb0f29cd960b5
TSwiftie <tswiftie@foxmail.com>
2020-09-26 19:48:13 +0000

    add cf/672Div2 a b c

---
+++ cf/672Div2/a.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+//#define int long long
+#define lowbit(x) (x&-x)
+#define SZ(x) ((int)x.size())
+#define all(x) x.begin(),x.end()
+#define lc (o<<1)
+#define rc (o<<1|1)
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+//mt19937 rnd(time(0));
+typedef long double ld;
+typedef long long LL;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<LL> vll;
+typedef pair<int,int> pii;
+typedef pair<LL,LL> pll;
+const int INF = 0x3f3f3f3f;
+const LL INF_ll = 0x3f3f3f3f3f3f3f3fLL;
+const double PI = acos(-1.0);
+const double EPS = 1e-8;
+const int N = 5e4+10;
+int a[N],n;
+void solve(){
+    scanf("%d",&n);for(int i = 1;i <= n;++i) scanf("%d",a+i);
+    a[n+1] = INF;int st = 1;
+    while(a[st] > a[st+1]) ++st;
+    if(st==n) printf("NO\n");
+    else printf("YES\n");
+}
+signed main(void){
+    int t;for(scanf("%d",&t);t--;) solve();
+    return 0;
+}
---
+++ cf/672Div2/b.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <ext/rope>
+#define int long long
+#define lowbit(x) (x&-x)
+#define SZ(x) ((int)x.size())
+#define all(x) x.begin(),x.end()
+#define lc (o<<1)
+#define rc (o<<1|1)
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+//mt19937 rnd(time(0));
+typedef long double ld;
+typedef long long LL;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<LL> vll;
+typedef pair<int,int> pii;
+typedef pair<LL,LL> pll;
+const int INF = 0x3f3f3f3f;
+const LL INF_ll = 0x3f3f3f3f3f3f3f3fLL;
+const double PI = acos(-1.0);
+const double EPS = 1e-8;
+const char *INPUT = "/home/ts/code/in.in";
+const int N = 1e5+10;
+int n,a[N];
+void solve(){
+    map<int,int>mp;int ans = 0;
+    scanf("%lld",&n);for(int i = 1,x;i <= n;++i){
+        scanf("%lld",&x);int cnt = 0;while(x){
+            ++cnt;
+            x >>= 1;
+        }
+        a[i] = cnt;
+        ans += mp[a[i]];
+        ++mp[a[i]];
+    }
+    printf("%lld\n",ans);
+}
+signed main(void){
+    //freopen(INPUT, "r", stdin);
+    int t;for(scanf("%lld",&t);t--;) solve();
+    return 0;
+}
