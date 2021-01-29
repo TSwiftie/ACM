1fb8a8c8c7579f4456b0ee54b5770035cd0d9511
TSwiftie <tswiftie@foxmail.com>
2021-01-29 13:27:16 +0000

    update

--- lc/1631.cpp
+++ lc/1631.cpp
@@ -2,32 +2,35 @@
 using namespace std;
 class Solution {
 public:
+    bool vis[105][105];
     const int dir[4][2] = {-1,0,0,-1,1,0,0,1};
+    bool dfs(const int &x,const int &y,const int &cha,const int &n,const int &m,const vector<vector<int>> &heights){
+        if(x==n-1 && y== m-1) return true;
+        bool ok = false;
+        for(int i = 0;i < 4 && !ok; ++i){
+            int dx = x + dir[i][0];
+            int dy = y + dir[i][1];
+            if(dx < 0 || dx >= n || dy < 0 || dy >= m) continue;
+            if(!vis[dx][dy] && abs(heights[dx][dy]-heights[x][y]) <= cha){
+                vis[dx][dy] = true;
+                ok = dfs(dx,dy,cha,n,m,heights);
+            }
+        }
+        return ok;
+    }
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n = heights.size(),m = heights[0].size();
         int left = 0, right = 999999, ans = 0;
         while (left <= right) {
             int mid = (left + right) / 2;
-            queue<pair<int,int>> q;
-            vector<bool>vis(m*n,false);
-            vis[0] = true;
-            q.push({0,0});
-            while(q.size()){
-                int x = q.front().first,y = q.front().second;q.pop();
-                if(x==n-1 && y==m-1) break;
-                for(int i = 0;i < 4;++i){
-                    int dx = x + dir[i][0]; 
-                    int dy = y + dir[i][1];
-                    int index = dx * m + dy;
-                    if(dx < 0 || dx >= n || dy < 0 || dy >= m) continue;
-                    if(!vis[index] && abs(heights[dx][dy]-heights[x][y]) <= mid){
-                        vis[index] = true;
-                        q.push({dx,dy});
-                    }
-                }
+            memset(vis,0,sizeof vis);
+            if (dfs(0,0,mid,n,m,heights)) {
+                ans = mid;
+                right = mid - 1;
             }
-            if (vis[n * m - 1]) ans = mid,right = mid - 1;
-            else left = mid + 1;
+            else {
+                left = mid + 1;
+            }
         }
         return ans;
     }
