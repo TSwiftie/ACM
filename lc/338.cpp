691f84fee10d7adfc76055c4661957eb074eede5
TSwiftie <tswiftie@foxmail.com>
2021-03-03 21:41:50 +0000

    update something

--- lc/338.cpp
+++ lc/338.cpp
@@ -3,15 +3,10 @@ using namespace std;
 class Solution {
 public:
     vector<int> countBits(int num) {
-        int tot = 0,x;
-        vector<int>ans;
+        int tot = 0,x;vector<int>ans;
         for(int i = 0;i <= num;++i){
-            tot = 0;
-            x = i;
-            while(x){
-                tot += (x%2);
-                x >>= 1;
-            }
+            tot = 0;x = i;
+            while(x){ tot += (x%2);x >>= 1; }
             ans.push_back(tot);
         }
         return ans;
