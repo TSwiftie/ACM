2847588c4e07a873aaef16876d4422722d76167c
TSwiftie <tswiftie@foxmail.com>
2021-03-12 15:38:26 +0000

    update

--- lc/331.cpp
+++ lc/331.cpp
@@ -3,14 +3,24 @@ using namespace std;
 class Solution {
 public:
     bool isValidSerialization(string preorder) {
-        int n = preorder.length(),i = 0,slots = 1;
+        int n = preorder.length();
+        int i = 0;
+        int slots = 1;
         while (i < n) {
-            if (slots == 0) return false;
-            if (preorder[i] == ',') --i;
-            else if (preorder[i] == '#') --slots,++i;
-            else {
-                while (i < n && preorder[i] != ',') i++;
-                ++slots;
+            if (slots == 0) {
+                return false;
+            }
+            if (preorder[i] == ',') {
+                i++;
+            } else if (preorder[i] == '#'){
+                slots--;
+                i++;
+            } else {
+                // 读一个数字
+                while (i < n && preorder[i] != ',') {
+                    i++;
+                }
+                slots++; // slots = slots - 1 + 2
             }
         }
         return slots == 0;
